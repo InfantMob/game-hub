f94334ca62fd7db3a7ef624bc3c6a4cae07a0c6d
Changhao <wuchan9hao@gmail.com>
2024-07-19 10:53:27 -0400

    Build search input

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area="main">
-        <HStack spacing={5} paddingLeft={10} marginBottom={5}>
+        <HStack spacing={5} paddingLeft={2.5} marginBottom={5}>
           <PlatformSelector
             selectedPlatform={gameQuery.platform}
             onSelectPlatform={(platform) =>
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -18,7 +18,8 @@ const GameGrid = ({ gameQuery }: Props) => {
       {error && <Text>{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        paddingLeft={10}
+        paddingLeft={2.5}
+        paddingRight={2.5}
         spacing={3}
       >
         {isLoading &&
