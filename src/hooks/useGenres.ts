ab53332258c537388b1dc483732906e653172476
Changhao <wuchan9hao@gmail.com>
2024-07-27 17:45:10 -0400

    Fetching Platforms

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -13,7 +13,7 @@ const useGenres = () =>
     queryKey: ["genres"],
     queryFn: () =>
       apiClient.get<FetchResponse<Genre>>("/genres").then((res) => res.data),
-    staleTime: 24 * 60 * 60 * 1000,
+    staleTime: 24 * 60 * 60 * 1000, //24h
     initialData: { count: genres.length, results: genres },
   });
 
